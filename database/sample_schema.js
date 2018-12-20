e2f000f3c164234d2ef0b4f4a83e02df6796e99c
Linden Chiu <hchiu87@gmail.com>
2018-12-20 15:50:52 -0800

    env setup

---
+++ database/sample_schema.js
@@ -0,0 +1,63 @@
+{
+  wid: INT,
+  reviewer: STRING, 
+  stars: NUMBER,
+  date_posted: DATE,
+  review_id, INT
+  review_header, STRING,  
+  review_body, STRING,
+  upvotes: INT,
+  downvotes: INT
+}
+
+const dummy_reviews = [
+{
+  wid: 100,
+  reviewer: "George Qian", 
+  stars: 5,
+  date_posted: "12-20-18",
+  review_id: 6578,
+  header, "Greatest watch ever",  
+  body, "I love this watch, it's the most amazing thing. 10/10, will buy a dozen more just like it.",
+  upvotes: 6,
+  downvotes: 0
+},
+
+{
+  wid: 100,
+  reviewer: "Cherri Hartigan", 
+  stars: 4,
+  date_posted: "12-23-18",
+  review_id: 9675,
+  header, "Not bad",  
+  body, "It gets the job done.",
+  upvotes: 9,
+  downvotes: 1
+},
+
+{
+  wid: 100,
+  reviewer: "Aaron Deane", 
+  stars: 5,
+  date_posted: "12-21-18",
+  review_id: 9275,
+  header, "The best purchase I ever made.",  
+  body, "It's great",
+  upvotes: 4,
+  downvotes: 2
+},
+
+{
+  wid: 100,
+  reviewer: "Linden Chiu", 
+  stars: 5,
+  date_posted: "10-21-18",
+  review_id: 9275,
+  header, "Cool",  
+  body, "!!!!!",
+  upvotes: 0,
+  downvotes: 16
+}
+]
+
+
