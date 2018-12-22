025fefd5d47e4bd716e86684dafff5b0a964808d
Linden Chiu <hchiu87@gmail.com>
2018-12-21 18:25:28 -0800

    dummy render

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,12 +3,62 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 import parseUrl from 'parse-url';
 
+const dummyShowcaseReviews = [
+{
+  _id: 1001,
+  w_id: 100,
+  reviewer: "George Qian", 
+  stars: 5,
+  date_posted: new Date("<1987-10-20>"),
+  review_header: "Greatest watch ever",  
+  review_body: "I love this watch, it's the most amazing thing. 10/10, will buy a dozen more just like it.",
+  upvotes: 6,
+  downvotes: 0
+},
+
+{
+  _id: 1002,
+  w_id: 100,
+  reviewer: "Cherri Hartigan", 
+  stars: 4,
+  date_posted: new Date("<1987-10-21>"),
+  review_header: "Not bad",  
+  review_body: "It gets the job done.",
+  upvotes: 9,
+  downvotes: 1
+},
+
+{
+  _id: 1003,
+  w_id: 100,
+  reviewer: "Aaron Deane", 
+  stars: 5,
+  date_posted: new Date("<1987-10-22>"),
+  review_header: "The best purchase I ever made.",  
+  review_body: "It's great",
+  upvotes: 4,
+  downvotes: 2
+},
+
+{
+  _id: 1004,
+  w_id: 100,
+  reviewer: "Linden Chiu", 
+  stars: 5,
+  date_posted: new Date("<1987-10-23>"),
+  review_header: "Cool",  
+  review_body: "!!!!!",
+  upvotes: 0,
+  downvotes: 16
+}
+];
+
 class ReviewsContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
     	id: null,
-    	reviews: null
+    	reviews: dummyShowcaseReviews
     };
   }
 
@@ -21,14 +71,15 @@ class ReviewsContainer extends React.Component {
     const successFn = reviews => {
       this.setState({
         id: wid,
-        reviews: reviews
+        reviews: reviews.data
       });
+   
     };
 
-    axios.get('/api/watches/:wid/reviews')
+    axios.get(`/api/watches/${wid}/reviews`)
       .then(function (response) {
         successFn(response);
-        console.log(response);
+        
       })
       .catch(function (error) {
         console.log(error);
@@ -36,7 +87,20 @@ class ReviewsContainer extends React.Component {
   }
 
   render() {
-    return <h1> Hello World </h1>
+    return (
+       <div>
+         <h1>Reviews</h1>
+
+            <div className="reviews">
+              {this.state.reviews.map(function(review, index) {
+               return <div key={index}> {review.review_body} </div>
+              })}
+    
+            </div>
+          </div>
+          
+       
+      )
   }
 }
 
