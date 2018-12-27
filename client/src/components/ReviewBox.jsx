4153a92b9ece5c839a6bcd5b97ca478a83767e1f
Linden Chiu <hchiu87@gmail.com>
2018-12-26 22:57:59 -0800

    install styled components, render most dummy information

--- client/src/components/ReviewBox.jsx
+++ client/src/components/ReviewBox.jsx
@@ -1,3 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const ReviewBox = props => {
+  return (
+  	<div className="wrapper">
+  	  <div className="review-user">
+  	    {props.review.reviewer}
+  	    <span> VERIFIED BUYER </span>
+      </div>
+  	  <div className="review-date">{props.review.date_posted.toString()}</div>
+  	  <div className="review-title">{props.review.review_header}</div>
+  	  <div className="review-content-wrapper">
+  	    <div className="review-content">{props.review.review_body}</div>
+  	  </div>
+      <div className="review-votes"></div>
 
+  	</div>
+  	)
+}
+
+export default ReviewBox;
