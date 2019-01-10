908c841d95d39bccb9ba3078d089b685cfb504cb
Paloma Velazquez <paloma.velazquez@gmail.com>
2019-01-09 22:30:39 -0800

    Create CRUD routes

--- database/index.js
+++ database/index.js
@@ -1,22 +1,61 @@
 const mongoose = require('mongoose');
 const { Review } = require('./Review_schema.js')
 
-mongoose.connect('mongodb://172.17.0.1:27017/fec', { useNewUrlParser: true })
-  .catch(err => {
-    console.log(err)
-  });
+mongoose.connect('mongodb://localhost/fec', { useNewUrlParser: true });
 
 var db = mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-const getReviewsForId = (id, cb) => {
-  Review.find({ w_id: id }, (err, document) => {
+// CREATE
+const addReview = (id, cb) => {
+	var doc = {};
+	doc._id = id;
+	doc.w_id = null;
+	doc.reviewer = null;
+	doc.stars = null;
+	doc.date_posted = null;
+	doc.review_id = null;
+	doc.review_header = null;
+	doc.review_body = null;
+	doc.upvotes = null;
+	doc.downvotes = null;
+	Review.insertOne(doc, (err, document) => {
+		if (err) { throw err }
+		cb();
+	})
+}
+
+// READ
+const getReviewsForId = (watch_id, cb) => {
+  Review.find({ 'w_id': watch_id }, (err, document) => {
     if (err) { throw err; }
     cb(document);
-  })
+  }).limit(10);
 };
 
+// UPDATE
+const editReview = (review_id, update, cb) => {
+	Review.updateOne(
+		{ '_id': review_id }, // Filter
+		{ 'review_body': update }, // Update
+		(err, document) => {
+			if (err) { throw err; }
+			cb(document);
+	})
+}
+
+// DELETE
+const deleteReview = (review_id, cb) => {
+	Review.deleteOne({_id: review_id}, (err, document) => {
+		if (err) { throw err; }
+		cb(document);
+	})
+}
+ 
+module.exports.addReview = addReview;
 module.exports.getReviewsForId = getReviewsForId;
+module.exports.editReview = editReview;
+module.exports.deleteReview = deleteReview;
 module.exports.database = db;
