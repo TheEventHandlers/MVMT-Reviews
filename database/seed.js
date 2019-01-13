5a01e41970cd9c823bd8c33e5d05dfce9a02533a
Paloma Velazquez <paloma.velazquez@gmail.com>
2019-01-13 13:35:20 -0800

    Generate large amounts of data and export to CSV

--- database/seed.js
+++ database/seed.js
@@ -10,44 +10,50 @@ const randomIntFromInterval = (min, max) =>// min and max included
   return Math.floor(Math.random()*(max-min+1)+min);
 }
 
-const generateReviews = (numberOfWatches) => {
-	const firstWatchId = 101;
-  let nextReviewId = 1001; // auto-increment
-  let reviewArray = [];
+const firstWatchId = 101;
+const lastWatchId = 199;
+let nextReviewId = 1001; // auto-increment
 
-  for (let watchId = firstWatchId; watchId <= numberOfWatches + firstWatchId; watchId += 1) {
-    let reviewsForThisWatch = randomIntFromInterval(4, 100);
-    for (let review = 1; review <= reviewsForThisWatch; review += 1) {
-      const record = {};
-      record._id = nextReviewId;
-      record.w_id = watchId;
-      record.reviewer = faker.name.findName();
-      record.stars = faker.random.number({
-        'min': 1,
-        'max': 5
-      });
-      record.date_posted = faker.date.past();
-      record.review_header = faker.lorem.words();
-      record.review_body = faker.lorem.paragraphs();
-      record.upvotes = faker.random.number(100);
-      record.downvotes = faker.random.number(100);
-      nextReviewId++;
+const generateCSVReview = () => {
+	const record = {};
+	record._id = nextReviewId;
+	record.w_id = randomIntFromInterval(firstWatchId, lastWatchId);
+	record.reviewer = faker.name.findName();
+	record.stars = faker.random.number({
+	  'min': 1,
+	  'max': 5
+	});
+	record.date_posted = faker.date.past();
+	record.review_header = faker.lorem.words();
+	record.review_body = faker.lorem.paragraphs();
+	record.upvotes = faker.random.number(100);
+	record.downvotes = faker.random.number(100);
+	nextReviewId++;
 
-    	reviewArray.push(record);
-    }
-  }
-  return reviewArray;
+  return json2csv(record, {header: false});
 };
 
-const fields = ['_id', 'w_id', 'reviewer', 'stars', 'date_posted', 'review_header', 'review_body', 'upvotes', 'downvotes'];
-const opts = { fields };
 
-// 192500 = 10 million / 52 reviews on average
-const csv = json2csv(generateReviews(100), opts);
+const writeStream = fs.createWriteStream(path.join(__dirname, '/data.csv'), { flags: 'w' });
+writeStream.write(`_id, w_id, reviewer, stars, date_posted, review_header, review_body, upvotes, downvotes\n`);
 
-// const writeStream = fs.createWriteStream(path.join(__dirname, '/data.csv'), { flags: 'w' });
+const writeData = function(entriesTodo) {
+	let ok = writeStream.write(generateCSVReview() + '\n')
 
-fs.writeFile(path.join(__dirname, '/data.csv'), csv, (err) => {  
-    if (err) throw err;
-    console.log('Data seeding complete!');
-});
+	if (entriesTodo < 1) {
+		return true;
+	}
+
+	if (ok) {
+		console.log(`Wrote an entry. ${entriesTodo - 1} reviews to go!`);
+		writeData(entriesTodo - 1);
+	} else {
+		writeStream.once('drain', () => {
+			console.log(`Drained the buffer. ${entriesTodo - 1} reviews to go!`);
+      writeData(entriesTodo - 1);
+    });
+	}
+}
+
+// 10 million
+writeData(10000000);
